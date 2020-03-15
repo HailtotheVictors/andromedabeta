a4ad1bac64a745c8a598f02bf24b3d4ea11ae7b6
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-03-15 09:29:02 -0600

    Add files via upload

---
+++ songtest.js
@@ -0,0 +1,15 @@
+var songInfo = [];
+songInfo[0] = [];
+songInfo[0][0] = ["Bad_Liar","Bad Liar","Imagine Dragons","/albums/Bad_Liar.jpg","[Single] (2018)"];
+songInfo[0][1] = ["Blossom","Blossom","Milky Chance","/albums/Blossom.jpeg","Blossom (2017)"];
+songInfo[0][2] = ["Bones","Bones","Galantis ft. OneRepublic","/albums/Bones.jpg","[Single] (2019)"];
+songInfo[0][3] = ["lonely_together","Lonely Together","Avicii ft. Rita Ora","/albums/avici.jpg","AVICI (2019)"];
+songInfo[0][4] = ["Cocoon","Cocoon","Milky Chance","/albums/Cocoon.jpg","[Single] (2016)"];
+
+songInfo[1] = [];
+songInfo[1][0] = ["Bad_Liar","Bad Liar","Imagine Dragons","/albums/Bad_Liar.jpg","[Single] (2018)"];
+songInfo[1][1] = ["Bones","Bones","Galantis ft. OneRepublic","/albums/Bones.jpg","[Single] (2019)"];
+songInfo[1][2] = ["Hey_Brother","Hey Brother","Avicii","/albums/Hey_Brother.jpg","[Single] (2013)"];
+songInfo[1][3] = ["Natural","Natural","Imagine Dragons","/albums/Natural.jpg","[Single] (2018)"];
+songInfo[1][4] = ["levels","Levels","Avicii","/albums/levelsremix.jpg","[Single] (2011)"];
+songInfo[1][5] = ["Dreamer","Dreamer","Axwell /\\ Ingrosso","/albums/More_Than_You_Know.jpg","More Than You Know (2017)"];
