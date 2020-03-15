d2ba792a2213be7a08ca4cf08b47760d0c04dd6e
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-03-15 09:37:44 -0600

    Update songtest.js

--- songtest.js
+++ songtest.js
@@ -1,15 +1,9 @@
 var songInfo = [];
 songInfo[0] = [];
-songInfo[0][0] = ["Bad_Liar","Bad Liar","Imagine Dragons","/albums/Bad_Liar.jpg","[Single] (2018)"];
-songInfo[0][1] = ["Blossom","Blossom","Milky Chance","/albums/Blossom.jpeg","Blossom (2017)"];
-songInfo[0][2] = ["Bones","Bones","Galantis ft. OneRepublic","/albums/Bones.jpg","[Single] (2019)"];
-songInfo[0][3] = ["lonely_together","Lonely Together","Avicii ft. Rita Ora","/albums/avici.jpg","AVICI (2019)"];
-songInfo[0][4] = ["Cocoon","Cocoon","Milky Chance","/albums/Cocoon.jpg","[Single] (2016)"];
+songInfo[0][0] = ["ibetmylife","I Bet My Life","Imagine Dragons","/albums/ibetmylife.jpg","[Single] (2014)"];
+songInfo[0][1] = ["thunder","Thunder","Imagine Dragon","/albums/thunder.jpg","[Single] (2017)"];
+songInfo[0][2] = ["warriors","Warriors","Imagine Dragons","/albums/warriors.jpg","[Single] (2014)"];
+songInfo[0][3] = ["whateverittakes","Whatever It Takes","Imagine Dragons","/albums/whateverittakes.jpg","[Single] (2017)"];
+songInfo[0][4] = ["happier","Happier","Marshmello ft. Bastille","/albums/happier.jpg","[Single] (2018)"];
 
 songInfo[1] = [];
-songInfo[1][0] = ["Bad_Liar","Bad Liar","Imagine Dragons","/albums/Bad_Liar.jpg","[Single] (2018)"];
-songInfo[1][1] = ["Bones","Bones","Galantis ft. OneRepublic","/albums/Bones.jpg","[Single] (2019)"];
-songInfo[1][2] = ["Hey_Brother","Hey Brother","Avicii","/albums/Hey_Brother.jpg","[Single] (2013)"];
-songInfo[1][3] = ["Natural","Natural","Imagine Dragons","/albums/Natural.jpg","[Single] (2018)"];
-songInfo[1][4] = ["levels","Levels","Avicii","/albums/levelsremix.jpg","[Single] (2011)"];
-songInfo[1][5] = ["Dreamer","Dreamer","Axwell /\\ Ingrosso","/albums/More_Than_You_Know.jpg","More Than You Know (2017)"];
