7c0b68e9274012fa82d63f7506c569234d9f734b
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-03-16 08:18:35 -0600

    Update songtest.js

--- songtest.js
+++ songtest.js
@@ -7,3 +7,8 @@ songInfo[0][3] = ["whateverittakes","Whatever It Takes","Imagine Dragons","/albu
 songInfo[0][4] = ["happier","Happier","Marshmello ft. Bastille","/albums/happier.jpg","[Single] (2018)"];
 
 songInfo[1] = [];
+songInfo[0][0] = ["ibetmylife","I Bet My Life","Imagine Dragons","/albums/ibetmylife.jpg","[Single] (2014)"];
+songInfo[0][1] = ["thunder","Thunder","Imagine Dragon","/albums/thunder.jpg","[Single] (2017)"];
+songInfo[0][2] = ["warriors","Warriors","Imagine Dragons","/albums/warriors.jpg","[Single] (2014)"];
+songInfo[0][3] = ["whateverittakes","Whatever It Takes","Imagine Dragons","/albums/whateverittakes.jpg","[Single] (2017)"];
+songInfo[0][4] = ["happier","Happier","Marshmello ft. Bastille","/albums/happier.jpg","[Single] (2018)"];
