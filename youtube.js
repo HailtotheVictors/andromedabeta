f58066eb93c5dd4853c2918549d99fb4f3f97f7a
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-03-16 08:18:59 -0600

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,4 +1,4 @@
-var version = "7.9";
+var version = "8.0";
 var playing = false;
 var currentPlaylist = 0;
 var sliderRun;
@@ -239,7 +239,6 @@ function advanceSlider() {
 			clearInterval(sliderRun);
 		}
 		songDisplay[0].innerHTML = toMins(currentTime);
-		console.log("currentTime:runTime::" + currentTime + ":" + runTime);
 	}, 50);
 }
 
