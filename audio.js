f0bea2a571b86711c0c3e0d1dcf2eda4cd867556
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-06-05 10:59:14 -0600

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,42 @@
+var context;
+var audio;
+var playButton;
+var audioElement;
+var bass = 10;
+var treble = 5;
+function conx() {
+audio = new Audio();
+	audio.src = 'vhl.mp3';
+	audio.controls = true;
+	audio.autoplay = true;
+	document.body.appendChild(audio);
+
+	context = new AudioContext();
+	var analyser = context.createAnalyser();
+
+	// Wait for window.onload to fire. See crbug.com/112368
+	//window.addEventListener('load', function(e) {
+	  // Our <audio> element will be the audio source.
+		var source = context.createMediaElementSource(audio);
+		source.connect(analyser);
+		analyser.connect(context.destination);
+		gainNode = context.createGain();
+		//??
+		bassFilter = context.createBiquadFilter();
+		bassFilter.type = "lowshelf"; 
+		bassFilter.frequency.value = 200;  // switches to 400 in UI
+		bassFilter.gain.value = bass;  // you'll need to hook this to UI too
+
+		trebleFilter = context.createBiquadFilter();
+		trebleFilter.type = "highshelf"; 
+		trebleFilter.frequency.value = 2000;  // switches to 6000 in UI
+		console.log(trebleFilter.frequency.value);
+		trebleFilter.gain.value = treble;  // you'll need to hook this to UI too
+
+		source.connect(bassFilter); 
+		bassFilter.connect(trebleFilter); 
+		trebleFilter.connect(context.destination);
+
+	  // ...call requestAnimationFrame() and render the analyser's output to canvas.
+	//}, false);
+}
