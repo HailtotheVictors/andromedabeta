f88c43ec84a4b8e82fb656d051257e1b837bb14f
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-06-05 10:41:32 -0600

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,85 @@
+var songList = [];
+
+//b
+songList[0]  = ["Bad_Liar","Bad Liar","Imagine Dragons","/albums/Bad_Liar.jpg","[Single] (2018)"];
+songList[1]  = ["believer","Believer","Imagine Dragons","/albums/believer.jpg","[Single] (2017)"];
+songList[2]  = ["betterthanever","Better Than Ever","Flight Facilites ft. Aloe Blacc","/albums/better.png","[Single] (2019)"];
+songList[3]  = ["Bleeding_Out","Bleeding Out","Imagine Dragons","/albums/Night_Visions.jpg","Night Visions (2012)"];
+songList[4]  = ["Blossom","Blossom","Milky Chance","/albums/Blossom.jpeg","Blossom (2017)"];
+songList[5]  = ["Bones","Bones","Galantis ft. OneRepublic","/albums/Bones.jpg","[Single] (2019)"];
+songList[6]  = ["boomerang","Boomerang","Imagine Dragons","/albums/Origins.jpg","Origins (2018)"];
+songList[7]  = ["Born_To_Be_Yours","Born to be Yours","Kygo & Imagine Dragons","/albums/Born_To_Be_Yours.jpg","[Single] (2018)"];
+songList[8]  = ["brokenarrows","Broken Arrows","Avicii","/albums/stories.jpg","Stories (2015)"];
+songList[9]  = ["bustthistown","Bust This Town","Stereophonics","/albums/kind.png","Kind (2019)"];
+
+//c
+songList[10] = ["centuroots","Centuroots","InanimateMashups","/albums/centuroots.png","[Single] (2018)"];
+songList[11] = ["Clouds","Clouds","Milky Chance","/albums/Blossom.jpeg","Blossom (2017)"];
+songList[12] = ["Cocoon","Cocoon","Milky Chance","/albums/Cocoon.jpg","[Single] (2016)"];
+songList[13] = ["coolout","Cool Out","Imagine Dragons","/albums/Origins.jpg","Origins (2018)"];
+
+//d,e,f,g
+songList[14] = ["demons","Demons","Imagine Dragons","/albums/Night_Visions.jpg","Night Visions (2013)"];
+songList[15] = ["Dreamer","Dreamer","Axwell /\\ Ingrosso","/albums/More_Than_You_Know.jpg","More Than You Know (2017)"];
+songList[16] = ["ego","Ego","Milky Chance","/albums/Blossom.jpeg","Blossom (2017)"];
+songList[17] = ["fades_away","Fades Away","Avicii ft. Noonie Bao","/albums/tim.jpg","Tim (2019)"];
+songList[18] = ["Flashed_Junk_Mind","Flashed Junk Mind","Milky Chance","/albums/Sadnecessary.jpg","Sadnecessary (2013)"];
+songList[19] = ["forabetterday","For a Better Day","Avicii","/albums/stories.jpg","Stories (2015)"];
+songList[20] = ["goodlife","Good Life","OneRepublic","/albums/wakingupblue.jpg","Waking Up (2009)"];
+
+//h,i,j
+songList[21] = ["happier","Happier","Marshmello ft. Bastille","/albums/happier.jpg","[Single] (2018)"];
+songList[22] = ["happyliar","Happy Liar","Adamusic","/albums/happyliar.png","[Single] (2018)"];
+songList[23] = ["heart_upon_my_sleeve","Heart Upon My Sleeve","Avicii ft. Imagine Dragons","/albums/tim.jpg","Tim (2019)"];
+songList[24] = ["ibetmylife","I Bet My Life","Imagine Dragons","/albums/ibetmylife.jpg","[Single] (2014)"];
+songList[25] = ["imsosorry","I'm So Sorry","Imagine Dragons","/albums/smokemirrors.jpg","Smoke + Mirrors (2015)"];
+songList[26] = ["imaginedragonsthemegamix","Imagine Dragons: The Megamix","InanimateMashups","/albums/themegamix.png","[Single] (2017)"];
+songList[27] = ["itstime","It's Time","Imagine Dragons","/albums/itstime.jpg","[Single] (2012)"];
+
+//l,m,n,o,p
+songList[28] = ["levels","Levels","Avicii","/albums/levelsremix.jpg","[Single] (2011)"];
+songList[29] = ["Little_Talks","Little Talks","Of Monsters and Men","/albums/myheadisananimal.jpg","My Head Is an Animal (2011)"];
+songList[30] = ["lonely_together","Lonely Together","Avicii ft. Rita Ora","/albums/avici.jpg","Av&#299;ci (2017)"];
+songList[31] = ["Machine","Machine","Imagine Dragons","/albums/Machine.jpg","[Single] (2018)"];
+songList[32] = ["More_Than_You_Know","More Than You Know","Axwell /\\ Ingrosso","/albums/More_Than_You_Know.jpg","[Single] (2017)"];
+songList[33] = ["Natural","Natural","Imagine Dragons","/albums/Natural.jpg","[Single] (2018)"];
+songList[34] = ["Only","Only","Imagine Dragons","/albums/Origins.jpg","Origins (2018)"];
+songList[35] = ["polaroid","Polaroid","Imagine Dragons","/albums/smokemirrors.jpg","Smoke + Mirrors (2015)"];
+songList[36] = ["Pompeii","Pompeii","Bastille","/albums/Pompeii.jpg","[Single] (2013)"];
+
+//r,s
+songList[37] = ["radioactivestars","Radioactive Stars","DylanGLC","/albums/radioactivestars.png","[Single] (2016)"];
+songList[38] = ["radioactivestarsreversed","Radioactive Stars (Reversed)","DylanGLC","/albums/radioactivestarsreversed.png","[Single] (2016)"];
+songList[39] = ["Renegades","Renegades","X Ambassadors","/albums/Renegades.jpg","[Single] (2015)"];
+songList[40] = ["riseup","Rise Up","Imagine Dragons","/albums/evolve.jpg","Evolve (2017)"];
+songList[41] = ["Roots","Roots","Imagine Dragons","/albums/Roots.jpg","[Single] (2015)"];
+songList[42] = ["Shots","Shots","Imagine Dragons","/albums/Shots_Broiler.jpg","[Single] (2015)"];
+songList[43] = ["Shots_Broiler","Shots (Broiler Remix)","Imagine Dragons","/albums/Shots_Broiler.jpg","[Single] (2015)"];
+songList[44] = ["smokeandmirrors","Smoke and Mirrors","Imagine Dragons","/albums/smokemirrors.jpg","Smoke + Mirrors (2015)"];
+songList[45] = ["Stolen_Dance","Stolen Dance","Milky Chance","/albums/Stolen_Dance.jpg","[Single] (2012)"];
+songList[46] = ["Stolen_Dance_Flic_Flac","Stolen Dance (Flic Flac Edit)","Milky Chance","/albums/Stolen_Dance_Flic_Flac.jfif","[Single] (2013)"];
+songList[47] = ["sunsetjesus","Sunset Jesus","Avicii","/albums/stories.jpg","Stories (2015)"];
+
+//t,v
+songList[48] = ["thedays","The Days","Avicii","/albums/thedays.jpg","[Single] 2014"];
+songList[49] = ["The_Nights","The Nights","Avicii","/albums/The_Nights.jpg","[Single] (2014)"];
+songList[50] = ["thunder","Thunder","Imagine Dragon","/albums/thunder.jpg","[Single] (2017)"];
+songList[51] = ["tiptoe","Tiptoe","Imagine Dragons","/albums/Night_Visions.jpg","Night Visions (2013)"];
+songList[52] = ["Two_High","Two High","Moon Taxi","/albums/Two_High.jpg","[Single] (2017)"];
+songList[53] = ["vivalapompeii","Viva la Pompeii","Dakota Mashups","/albums/vivalapompeii.png","[Single] (2014)"];
+songList[54] = ["vivalavidaclocks","Viva la Vida/Clocks","Infinity Remix","/albums/vivalavidaclocks.png","[Single] (2016)"];
+
+//w
+songList[55] = ["Waiting_for_Love","Waiting for Love","Avicii","/albums/Waiting_for_Love.jpg","[Single] (2015)"];
+songList[56] = ["wakemeup","Wake Me Up","Avicii","/albums/wakemeupalbum.png","[Single] (2013)"];
+songList[57] = ["walkingthewire","Walking the Wire","Imagine Dragons","/albums/walkingthewire.jpg","[Single] (2017)"];
+songList[58] = ["warriorblood","Warrior Blood","InanimateMashups","/albums/warriorblood.png","[Single] (2018)"];
+songList[59] = ["warriors","Warriors","Imagine Dragons","/albums/warriors.jpg","[Single] (2014)"];
+songList[60] = ["westcoast","West Coast","Imagine Dragons","/albums/Origins.jpg","Origins (2018)"];
+songList[61] = ["whateverittakes","Whatever It Takes","Imagine Dragons","/albums/whateverittakes.jpg","[Single] (2017)"];
+songList[62] = ["withoutyou","Without You","Avicii","/albums/avici.jpg","Av&#299;ci (2017)"];
+songList[63] = ["whoweare","Who We Are","Imagine Dragons","/albums/smokemirrors.jpg","Smoke + Mirrors (2015)"];
+songList[64] = ["Wish_I_Knew_You","Wish I Knew You","The Revivalists","/albums/Men_Amongst_Mountains.jpg","Men Amongst Mountains (2016)"];
+
+//y
+songList[65] = ["youmakeme","You Make Me","Avicii","/albums/true.jpg","True (2013)"];
