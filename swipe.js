b87d50e685e9ffd683e1e8e7c4c00c71052308b3
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-06-06 17:00:31 -0600

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -8,43 +8,42 @@ function startTouch(e) {
 };
  
 function moveTouch(e) {
-  if (initialX === null) {
-    return;
-  }
+	if (initialX === null) {
+		return;
+	}
+	if (initialY === null) {
+		return;
+	}
  
-  if (initialY === null) {
-    return;
-  }
+	var currentX = e.touches[0].clientX;
+	var currentY = e.touches[0].clientY;
  
-  var currentX = e.touches[0].clientX;
-  var currentY = e.touches[0].clientY;
+	var diffX = initialX - currentX;
+	var diffY = initialY - currentY;
  
-  var diffX = initialX - currentX;
-  var diffY = initialY - currentY;
+	if (Math.abs(diffX) > Math.abs(diffY)) {
+		// sliding horizontally
+		if (diffX > 0) {
+			// swiped left
+			console.log("swiped left");
+			nextSong();
+		} else {
+			// swiped right
+			console.log("swiped right");
+			prevSong();
+		}  
+	} else {
+		// sliding vertically
+		if (diffY > 0) {
+			// swiped up
+			console.log("swiped up");
+		} else {
+			// swiped down
+			console.log("swiped down");
+		}  
+	}
  
-  if (Math.abs(diffX) > Math.abs(diffY)) {
-    // sliding horizontally
-    if (diffX > 0) {
-      // swiped left
-      console.log("swiped left");
-	  nextSong();
-    } else {
-      // swiped right
-      console.log("swiped right");
-    }  
-  } else {
-    // sliding vertically
-    if (diffY > 0) {
-      // swiped up
-      console.log("swiped up");
-    } else {
-      // swiped down
-      console.log("swiped down");
-    }  
-  }
- 
-  initialX = null;
-  initialY = null;
-   
-  e.preventDefault();
+	initialX = null;
+	initialY = null;
+	e.preventDefault();
 };
